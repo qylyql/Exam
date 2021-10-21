f900bcd64a44d20d049c325a68fa6720d52f8398
qiyanli <1289127268@qq.com>
2021-10-21 20:35:55 +0800

    third commit

---
+++ app/src/main/java/com/swufestu/examtally/UniteApp.java
@@ -0,0 +1,18 @@
+package com.swufestu.examtally;
+
+
+import android.app.Application;
+
+import com.swufestu.examtally.db.DBManager;
+
+/* 表示全局应用的类*/
+public class UniteApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // 初始化数据库
+        DBManager.initDB(getApplicationContext());
+    }
+}
+
